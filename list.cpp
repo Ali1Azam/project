d784f096246e34397ce214fff27f102ccd81b95a
Md Ali Azam <mdaliazam122@gmail.com>
2025-07-20 23:15:21 +0530

    Commit list.cpp and queue.cpp before rebase

--- list.cpp
+++ list.cpp
@@ -10,11 +10,11 @@ int main()
     a.push_front(6);
     a.push_back(7);
     a.push_front(6);
-
     
     for(auto i=a.begin();i!=a.end();i++){
         cout<<*i;
     }
+
     cout<<endl;
     
     a.pop_back();
